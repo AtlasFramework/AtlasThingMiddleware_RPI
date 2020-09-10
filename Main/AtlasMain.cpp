211b193581f7475bcf5f7b294e5aef0718125132
Ahmed_E_Khaled <ahmedeeldin@gmail.com>
2020-09-10 09:32:16 -0500

    Update AtlasMain.cpp

--- Main/AtlasMain.cpp
+++ Main/AtlasMain.cpp
@@ -11,23 +11,24 @@ int main(int argc, char *argv[]) {
 	//std::thread t1(&Communication_Manager::Communication_Manager,&CM); 	
 
 	//IP socket communication	
-	//std::thread t1(&Communication_Manager::BroadcastTweets_IPSocket,&CM); 
-	//std::thread t2(&Communication_Manager::CaptureMatesTweets_IPSocket,&CM);
-	//std::thread t3(&Communication_Manager::CaptureAPIcalls_IPSocket,&CM);
+	std::thread t1(&Communication_Manager::BroadcastTweets_IPSocket,&CM); 
+	std::thread t2(&Communication_Manager::CaptureMatesTweets_IPSocket,&CM);
+	std::thread t3(&Communication_Manager::CaptureAPIcalls_IPSocket,&CM);
 
 	//MQTT socket communication
 
-	std::thread t4(&Communication_Manager::MQTT_Publish,&CM);
-	std::thread t5(&Communication_Manager::MQTT_Subscribe,&CM);
+	//std::thread t4(&Communication_Manager::MQTT_Publish,&CM);
+	//std::thread t5(&Communication_Manager::MQTT_Subscribe,&CM);
 
 	//Objects_Engine OE;	
-	//std::thread t4(&Objects_Engine::Start_DeviceManagement,&OE); 
+	//std::thread t6(&Objects_Engine::Start_DeviceManagement,&OE); 
 	
-	//t1.join();
-	//t2.join();
-	//t3.join();
-	t4.join();
-	t5.join();
+	t1.join();
+	t2.join();
+	t3.join();
+	//t4.join();
+	//t5.join();
+	//t6.join();
 
 	return 0;
 }
